e121d74a7fbbbea48e55d2e224c3be236b259cc3
LiangGeorge <geoliang01@gmail.com>
2021-10-02 01:01:58 -0400

    Added Item Loading

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,17 +1,33 @@
 import React from "react";
+import Item from "./Item";
 
 export default class Workspace extends React.Component {
     render() {
+        const {
+            currentList
+        } = this.props
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
                     <div id="edit-numbering">
+                       
                         <div className="item-number">1.</div>
+                        
                         <div className="item-number">2.</div>
+                    
                         <div className="item-number">3.</div>
+                        
                         <div className="item-number">4.</div>
+                        
+
                         <div className="item-number">5.</div>
                     </div>
+                    <div id="edit-items">
+                        {currentList ? currentList.items.map((item) => (<Item key={item} itemName={item} />)) : null } 
+                        {/* {currentList ? console.log(currentList.items) : null }  */}
+                    </div>
+
+                    
                 </div>
             </div>
         )
